007a5b6859e2c33eeb188245c3af9aefbffbbcf7
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-08-15 15:03:49 +0500

    add card random

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -5,8 +5,13 @@ import Leadership from "../../assets/images/leadership.png";
 import Scissors from "../../assets/images/scissors.png";
 import Card from "./Components/cards";
 import Button from "../../Components/Buttons";
+import { useState } from "react";
 
 const Main = () => {
+  const [activeCard, setActiveCard] = useState();
+  const [randomCard, setRandomCard] = useState();
+  const images = { Scissors: Scissors, Paper: Hand, Rock: Leadership };
+
   const card = [
     {
       img: Leadership,
@@ -28,10 +33,25 @@ const Main = () => {
     },
   ];
 
-  const startGame = (id) => {};
-  const handleCard = (name) => {
-    console.log(name);
+  const startGame = () => {
+    const random = Math.floor(Math.random() * card.length);
+    setRandomCard(card[random].img);
+
+    if (activeCard === card[random].name) {
+      console.log("you win");
+    } else {
+      console.log("you lose ");
+    }
+
+    console.log(card[random].name);
   };
+
+  // console.log(activeCard);
+
+  const handleCard = (name) => {
+    setActiveCard(name);
+  };
+
   return (
     <MainStyle>
       <Container>
@@ -44,17 +64,21 @@ const Main = () => {
           <div className="main__game-board-wrapper">
             <div className="main__game-persons">
               <div className="main__game-question-wrapper">
-                <div className="main__question-box-wrapper">?</div>
+                <div className="main__question-box-wrapper">
+                  {activeCard ? <img src={images[activeCard]} /> : "?"}
+                </div>
                 <p className="main__person-name"> You</p>
               </div>
               <p className="main__game-vs">VS</p>
               <div className="main__game-question-wrapper">
-                <div className="main__question-box-wrapper">?</div>
+                <div className="main__question-box-wrapper">
+                  {randomCard ? <img src={randomCard} /> : "?"}
+                </div>
                 <p className="main__person-name"> Computer</p>
               </div>
             </div>
             <Button onclick={startGame} variant={"green"}>
-              start
+              Play
             </Button>
           </div>
           <div className="main__game-list"></div>
